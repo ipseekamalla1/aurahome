53b0a0f6496bd3aadcc765de96c56d21a4370e63
Ipseeka Malla <ipseeka.malla1@gmail.com>
2024-11-20 22:58:11 -0500

    shop page created for user

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\AuthAdmin;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +17,7 @@ Route::get('/', [HomeController::class, 'index'])->name('home.index');
 // Routes for Authenticated Users
 Route::middleware(['auth'])->group(function () {
     Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
+    Route::get('/shop',[ShopController::class,'index'])->name('shop.index');
 });
 
 // Routes for Admin with Middleware
