b5e40773f3b203a9d5894487f33981a435046435
Ipseeka Malla <ipseeka.malla1@gmail.com>
2024-11-28 23:41:32 -0500

    title changed of home page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Aura Home-Your solution of complete home') }}</title>
+  <title>AuraHome-Your Complete Solution</title>
 
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <meta name="author" content="aura home decor" />
